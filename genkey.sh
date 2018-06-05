548e94afe377cd659e63775aa52eae140644baf9
hwlin1414 <hwlin1414@users.noreply.github.com>
2018-06-05 18:05:09 +0800

    Update genkey.sh

--- genkey.sh
+++ genkey.sh
@@ -3,3 +3,6 @@ exit
 openssl genrsa -out ssl.key 2048
 openssl req -new -sha256 -key ssl.key -out ssl.csr
 openssl x509 -req -days 365 -signkey ssl.key -in ssl.csr -out ssl.crt
+
+# sign from ca
+openssl x509 -req -in ssl.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out ssl.crt -days 365 -sha256
